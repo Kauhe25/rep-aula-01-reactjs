12edc0a3f35d7cf04d34d30a55597e73dd1d7cba
kauhe <kauhe25@gmail.com>
2025-01-16 19:32:57 -0300

    aula 2: sintaxe de jsx e tsx

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # rep-aula-01-reactjs
 
 ## Aula 01
-[X] Setup do React
+[X] Setup do React com Vite
+[X] Hot Reload
+[X] Limpando o projeto dos arquivos desnecessários
+[X] React Fragment
+[X] JSX/TSX
+[X] Interpolação de valores
+
+## Aula 02
--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -5,14 +5,29 @@ import './App.css'
 function App() {
 
   const welcome = <h1>Seja bem vindo</h1>
-
+  const curtindo = true;
+  const numero = 6;
+  const expressao = numero % 2 == 0? 'par' : 'impar';
+  const cursos = ['html','css','js'];
   return (
     // React Fragment
     <>
-    {/* Codigo Html */}
-    <h1>Aula Revisão</h1>
-    <h2>texto</h2>
-    {welcome}
+      {/* Codigo Html */}
+      <h1>Aula Revisão</h1>
+      <h2>texto</h2>
+      {welcome}
+      <h2>Introdução ao React</h2>
+      {curtindo ? 'Estou curtindo' : 'Não curti'}
+
+      <h1 className={expressao}>
+        {numero}</h1>
+
+      {curtindo && <div><h2>Estou curtindo, toop</h2></div> }
+    
+      {/* Exemplo de iteração */}
+      <p>{cursos.map((curso, posicao) => {
+        return <p>{curso} - {posicao}</p>
+      })}</p>
     </>
   )
 }
