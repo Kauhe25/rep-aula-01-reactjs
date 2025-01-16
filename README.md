1175983a6250849a62e859e5aeef7dff0bc53cd9
kauhe <kauhe25@gmail.com>
2025-01-16 15:58:38 -0300

    estrutura basica do react

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rep-aula-01-reactjs
+
+## Aula 01
+[X] Setup do React
---
+++ app-react/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+
+function App() {
+
+  const welcome = <h1>Seja bem vindo</h1>
+
+  return (
+    // React Fragment
+    <>
+    {/* Codigo Html */}
+    <h1>Aula Revisão</h1>
+    <h2>texto</h2>
+    {welcome}
+    </>
+  )
+}
+
+export default App
